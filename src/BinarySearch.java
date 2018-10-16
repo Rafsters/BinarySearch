730dcf0c42e6dcdf65e9f7ad9504bedf55c6357b
Rafal Kwiecien <r.kwiecien@polsource.com>
2018-10-16 12:00:20 +0200

    Updated console messages

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,33 +1,33 @@
 public class BinarySearch {
-    // Returns index of x if it is present in arr[],
-    // else return -1
+    // Method returns index of given element(x) if it is present in array(arr[]),
+    // If it is not present method returns -1
     public static int binarySearch(int arr[], int x) {
         int l = 0, r = arr.length - 1;
+        System.out.println("Array length: " + arr.length + ", given element x: " + x);
         while (l <= r)
         {
             int m = l + (r-l)/2;
-            System.out.println("Middle index: " + m);
+            System.out.println();
+            System.out.println("Middle element (Value: " + arr[m] + ". Index: " + m + ")");
             // Check if x is present at mid
             if (arr[m] == x)
                 return m;
 
-            System.out.println("arr[" + m + "] = " + arr[m] +  " < " + x + "? " + (arr[m] < x));
+            System.out.println("Comparing value of middle element (" + arr[m] + ") to given element x (" + x + ").");
+            System.out.println("arr[" + m + "]=" + arr[m] +  " < " + x + " - " + (arr[m] < x));
 
-            // If x greater, ignore left half
             if (arr[m] < x) {
 
                 l = m + 1;
-                System.out.println("Left half: " + l);
+                System.out.println("The value of middle element is less than x, so right half is ignored");
             }
-                // If x is smaller, ignore right half
             else{
                 r = m - 1;
-                System.out.println("Right half: " + r);
+                System.out.println("The value of middle element is greater than x, so left half is ignored");
             }
         }
 
-        // if we reach here, then element was
-        // not present
+        // Returns -1 if the element was not present in array
         return -1;
     }
 
--- src/BinarySearchApp.java
+++ src/BinarySearchApp.java
@@ -7,12 +7,13 @@ Group: KrDUIs2012Is
  */
 public class BinarySearchApp {
     public static void main(String args[]){
-        int arr[] = RandomIntGenerator.generateRandomArrayOfIntegers(15);
+        int arr[] = RandomIntGenerator.generateRandomArrayOfIntegers(25);
         int n = arr.length;
-        int x = 6;
+        int x = 34;
         int result = BinarySearch.binarySearch(arr, x);
+        System.out.println();
         if (result == -1)
-            System.out.println("Element not present");
+            System.out.println("Element " + x + " is not present in generated array. Try again!");
         else
             System.out.println("Element " + x + " found at " +
                     "index " + result);
